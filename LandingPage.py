a4686179b9e633b8add14d35232a6141fc703e6c
ckang21 <81390797+ckang21@users.noreply.github.com>
2021-10-31 17:25:06 -0600

    Add files via upload

---
+++ LandingPage.py
@@ -0,0 +1,50 @@
+import tkinter as tk
+from tkinter import messagebox as msg
+
+#Need to import Employee Profile Frame
+#through controller add the id to the data and have it populate the profile page.
+
+class LandingFrame(tk.Frame):
+    #Constructor class
+    def __init__(self, parent, controller):
+        #Initialize tk
+        tk.Frame.__init__(self, parent)
+
+        #Initialize controller
+        self.controller = controller
+        self.login_command = None
+        self.controller.app_data["LoginFrame_id"]
+        
+        #Set the format of the buttons and all of their locations
+        logout_button = tk.Button(self, text = "Logout", command = lambda: controller.present_frame("LoginFrame"))
+        logout_button.grid(row = 5, column = 3)
+        profile_button = tk.Button(self, text = "Go to Profile", command = lambda: controller.present_frame("EmployeeProfile"))
+        profile_button.grid(row = 2, column = 0)
+        employee_button = tk.Button(self, text ="Go to Employee Page", command = lambda: controller.present_frame("EmployeeList"))
+        employee_button.grid(row = 2, column = 5)
+
+    def set_user_id(self, id):
+        self.user_id = id
+
+    def set_login_command(self, command):
+        self.login_command = command
+    
+    #Once implemented this command will take the user to his personal profile
+    def on_profile_clicked(self):
+        #
+        if EmployeeProfile.check_permission(id) == true:
+            self.controller.present_frame("EmployeeProfile")
+
+    #This command will take a user with higher permission to a list of Employees page
+    def on_employee_clicked(self):
+        #Permission needs to be fully implemented for this to check admin level.
+        #if employee.get_employee_data(self.id, permission) == true:
+        self.controller.present_frame("EmployeeList")
+        
+        #else:
+        #    msg.showwarning(title="Insufficient Permission", message="Incorrect Permission Level")
+    
+    #This command will return the user to the login page and remove the current frame
+    def on_logout_clicked(self):
+        self.login_command()
+        #close LandingPage
---
+++ LoginPage.py
@@ -0,0 +1,45 @@
+import tkinter as tk
+from tkinter import messagebox as msg
+
+import auth
+
+#auth.read_auth_data(path)
+
+
+class LoginFrame(tk.Frame):
+    def __init__(self, parent, controller):
+        # ID and password text boxes plus labels, save inputs to variables for future use
+        # Set up the frame and create all buttons
+
+        # Initialize tk
+        tk.Frame.__init__(self, parent)
+
+        # Initialize controller
+        self.controller = controller
+        self.controller.add_data("LoginFrame_id", id)
+
+        id_label = tk.Label(self, text = "Employee ID").grid(row=3, column = 3)
+        self.e_id = tk.StringVar()
+        e_id_input = tk.Entry(self, textvariable = self.e_id).grid(row = 3, column = 4)
+        password_label = tk.Label(self, text = "Password").grid(row = 3, column = 7)
+        self.password = tk.StringVar()
+        password_input = tk.Entry(self, textvariable = self.password, show = '*').grid(row = 3, column = 8)
+        
+        # Add login button, calls checkLogin
+        login_button = tk.Button(self, text = "Login", command = self.check_login).grid(row = 8, column = 5)
+
+    
+    def check_login(self):
+        """
+        results = auth.verify_password(self.e_id, self.password)
+        if results == auth.PASSWORD_CORRECT:
+            self.controller.present_frame("LandingFrame", ssef.e_id)
+            loginPage.removeLogin()
+        else:
+         self.failLogin()
+         """
+        pass
+        
+    # Will call up a warning box if the credentials are invalid
+    def fail_login(self):
+         msg.showwarning(title="Failed Login", message="Incorrect Username or Password")
